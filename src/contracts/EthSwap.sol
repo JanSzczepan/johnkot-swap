0ca8c17cbf44ff07dc4c6bc925f4e899496222b2
Jan Szczepan <janszczepan62@gmail.com>
2022-03-07 22:25:04 +0100

    Build up buyTokens function in EthSwap contract that allows user to buy Tokens

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,30 @@
 pragma solidity ^0.5.0;
 
+import './Token.sol';
+
 contract EthSwap {
    string public name = "JohnKotSwap Instant Exchange";
+   Token public token;
+   uint rate = 100;
+
+   event TokenPurchased (
+      address account,
+      address token,
+      uint amount,
+      uint rate
+   );
+
+   constructor(Token _token) public {
+      token = _token;
+   }
+
+   function buyTokens() public payable {
+      uint tokensAmount = msg.value * rate;
+
+      require(token.balanceOf(address(this)) >= tokensAmount);
+
+      token.transfer(msg.sender, tokensAmount);
+
+      emit TokenPurchased(msg.sender, address(token), tokensAmount, rate);
+   }
 }
