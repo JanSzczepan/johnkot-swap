8aa31294e6fe7f335a94ca8cddc1da0d574a081e
Jan Szczepan <janszczepan62@gmail.com>
2022-03-09 20:23:02 +0100

    Build up sellTokens function in EthSwap contract that allows user to sell Tokens

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -14,6 +14,13 @@ contract EthSwap {
       uint rate
    );
 
+   event TokenSold (
+      address account,
+      address token,
+      uint amount,
+      uint rate
+   );
+
    constructor(Token _token) public {
       token = _token;
    }
@@ -26,5 +33,19 @@ contract EthSwap {
       token.transfer(msg.sender, tokensAmount);
 
       emit TokenPurchased(msg.sender, address(token), tokensAmount, rate);
+   }
+
+   function sellTokens(uint _amount) public {
+      
+      require(token.balanceOf(msg.sender) >= _amount);
+
+      uint etherAmount = _amount / rate;
+
+      require(address(this).balance >= etherAmount);
+
+      token.transferFrom(msg.sender, address(this), _amount);
+      msg.sender.transfer(etherAmount);
+
+      emit TokenSold(msg.sender, address(token), _amount, rate);
    }
 }
